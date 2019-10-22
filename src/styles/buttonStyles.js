fdaf0f8d85251b455c87f7c17959f9619bbcb373
Jordan Fokoua <jfokoua@gmail.com>
2019-10-23 01:41:18 +0200

    :lipstick: Set Button color to primary

--- src/styles/buttonStyles.js
+++ src/styles/buttonStyles.js
@@ -1,8 +1,8 @@
 import { makeStyles } from '@material-ui/core/styles';
 
-const buttonStyles = makeStyles({
+const buttonStyles = makeStyles(theme=>({
   rectangularButton: {
-    background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+    background: theme.palette.primary.main,
     border: 0,
     borderRadius: 0,
     boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
@@ -10,6 +10,6 @@ const buttonStyles = makeStyles({
     height: 48,
     padding: '0.5em 1.5em',
   },
-});
+}));
 
 export default buttonStyles;
