60cd881639351fb2788e6ec9af52d4e92e5bd774
Jordan Fokoua <jfokoua@gmail.com>
2019-10-23 18:50:24 +0200

    :sparkles: REQUEST_PRODUCTS Action

---
+++ src/redux/actions/actions.js
@@ -0,0 +1,15 @@
+import { request } from '../../api';
+import { REQUEST_PRODUCTS } from './actionTypes';
+
+export const requestProducts = () => (dispatch, getState) => {
+  return request({ url: '/products', method: 'GET' })
+    .then(response => {
+      if (response.status === 200) {
+        return Promise.resolve(
+          dispatch({ type: REQUEST_PRODUCTS, payload: response.data })
+        );
+      }
+      return Promise.reject(new Error('Failed to get products'));
+    })
+    .catch(e => Promise.reject(new Error(e)));
+};
