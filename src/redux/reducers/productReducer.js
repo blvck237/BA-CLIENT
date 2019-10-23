24791d5793f4effe3edd2853fc7c76fb867a77de
Jordan Fokoua <jfokoua@gmail.com>
2019-10-23 23:41:47 +0200

    :fire:

--- src/redux/reducers/productReducer.js
+++ src/redux/reducers/productReducer.js
@@ -1,24 +1,13 @@
 import { REQUEST_PRODUCTS } from '../actions/actionTypes';
 
-const initialState = {
-};
+const initialState = {};
 
 const products = (state = initialState, action) => {
-  console.log('Log: products -> action', action);
   switch (action.type) {
-    case 'persist/REHYDRATE':
-      if (action && action.payload && action.payload.i18n) {
-        return {
-          ...action.payload.i18n,
-          productssss: {},
-        };
-      }
-      return initialState;
-
     case REQUEST_PRODUCTS:
       return {
         ...state,
-        ...action.payload
+        ...action.payload,
       };
 
     default:
